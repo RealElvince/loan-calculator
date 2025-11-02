4090e62fbe6ccb28e80ad2b370d5e8e25b5caa5a
RealElvince <obuongely@yahoo.com>
2025-11-02 13:23:11 +0300

    a function to calculate total interest

--- calculate_loan.py
+++ calculate_loan.py
@@ -3,3 +3,7 @@ class LoanCalculator:
         self.principal = principal
         self.interest_rate = interest_rate
         self.years = years
+    
+    def total_interest(self):
+        interest = self.principal*(self.interest_rate/100)*self.years
+        return interest
